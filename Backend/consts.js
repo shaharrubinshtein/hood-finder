46f77a104cb5cc460cb37d9e675153b6e57cfacf
shaharrubinshtein <tody0489@gmail.com>
2023-12-20 21:50:09 +0200

    remove consts file

--- Backend/consts.js
+++
@@ -1,5 +0,0 @@
-module.exports={
-    CONNECTION_STRING:'mongodb+srv://hoodDb:vUes935fjPZemsK7@cluster0.qowpxfo.mongodb.net/hoodDb?retryWrites=true&w=majority',
-    DB_USER:'hoodDb',
-    DB_PASS:'vUes935fjPZemsK7'
-}
